dcd9573ec3b40245ed016cf38d27100c3be0744c
Leo Zhang <ring_lochid@outlook.com>
2025-11-22 19:34:59 +1100

    0.5

---
+++ script.js
@@ -0,0 +1,57 @@
+const copy_btn = document.getElementById('copy_btn');
+const range_form = document.getElementById('pwd_len_control');
+const checkboxs = document.querySelectorAll('.checkbox');
+const form_btn = document.querySelector('.generate_btn');
+    
+const strength_value_boxes = document.querySelectorAll('.strength_value_box');
+const pwd_len = document.querySelector('.length_value');
+const pwd_res = document.querySelector('.res');
+
+const digits = '0123456789';
+const lowercase_letter = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase_letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const special_symbols = '~!@#$%^&*()_<>?';
+
+const alpha_dict = {
+    '0': digits,
+    '1': lowercase_letter,
+    '2': uppercase_letter,
+    '3': special_symbols
+};
+
+function generate_rn(pwd_length, mod){
+    const res = [];
+    const bar = 65536 - 65536%mod;
+    while (res.length < pwd_length){
+        const tmp = Uint16Array(1);
+        globalThis.crypto.getRandomValues(tmp);
+        if (tmp[0] >= bar){
+            continue;
+        }
+        res.push(tmp[0]%mod);
+    }
+    return res;
+}
+
+function generate_pwd(){
+    const alphabet = '';
+    const checked = [];
+    for(let i = 0; i < checkboxs.length; i++){
+        if (checkboxs[i].ischecked){
+            checked.push(true);
+        }
+        else{checked.push(false);}
+    }
+    if (checked.every((e) => {return (e === false)})){throw new Error("Alphabet shouldn't be empty.");}
+    for (let i = 0; i < checked.length; i++){
+        if (checked[i] === true){
+            alphabet += alpha_dict[i.toString()];
+        }
+    }
+    
+
+}
+
+function main(){
+
+}
